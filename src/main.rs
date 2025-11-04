ff27d280e2acef9fe1e419386591905a7e8a37dc
Froes <froes.ws@gmail.com>
2025-11-04 20:54:54 -0300

    WRK-0004 Fix

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use crossbow::DataFrame;
 fn main() {
     println!("Executando o binário de demonstração do Crossbow!"); //
 
-    let csv_path = "/home/froes/projetos/playground/employees.csv";
+    let csv_path = "employees.csv";
     println!("Tentando carregar '{}'...", csv_path);
 
-    // Tenta carregar o CSV.
-    // `read_csv` é a função que implementamos no DataFrame.
     match DataFrame::read_csv(csv_path) {
         Ok(df) => {
             println!("\n--- SUCESSO! ---");
@@ -16,17 +14,9 @@ fn main() {
                 df
             );
 
-            // `df` aqui é a sua struct DataFrame.
-            // O `Debug` print vai mostrar os nomes das colunas
-            // e os enums `SeriesData` internos (ex: Int64(Vec<Option<i64>>)).
-            println!("{:#?}", df); // O `#` no `{:#?}` faz um "pretty-print"
-
-            // Agora podemos interagir com ele
             println!("\nShape do DataFrame: {:?}", df.shape());
             println!("Nomes das colunas: {:?}", df.get_column_names());
 
-            // Vamos tentar um filtro
-            println!("\nFiltrando por 'age > 25'...");
             if let (Ok(_age_col), Ok(mask)) = (
                 df.select("age"),
                 df.select("age").and_then(|col| col.gt_i64(25)),
