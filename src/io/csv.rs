e5cd608778500807fe4af5f372f3915366288a20
froesws <froes.ws@gmail.com>
2026-04-27 22:58:22 -0300

    work-0019 - Remove unused I/O module stubs

--- src/io/csv.rs
+++
@@ -1,4 +0,0 @@
-//! CSV I/O operations
-//!
-//! Functions for reading and writing CSV files.
-//! Placeholder for future modularization.
--- src/io/parquet.rs
+++
@@ -1,4 +0,0 @@
-//! Parquet I/O operations
-//!
-//! Functions for reading and writing Parquet files.
-//! Placeholder for future modularization.
